b58d7f69b2407d47d26e5decf7074617861e7256
dev-mondoshawan <dev@mondoshawan.io>
2026-04-20 17:06:01 -0500

    Remove temporary scan scripts

--- run_tks.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set PATH=%PATH%;z:\Sentinal Engine\foundry-bin
-cd /d "z:\Sentinal Engine\sentinel-engine"
-python orchestrator.py --target "z:\Sentinal Engine\tokenkickstarter\smart-contracts" --report --project-name "TokenKickstarter Security Audit" > "z:\Sentinal Engine\tks_scan_output.txt" 2>&1
-echo EXIT_CODE: %ERRORLEVEL% >> "z:\Sentinal Engine\tks_scan_output.txt"
-echo DONE
